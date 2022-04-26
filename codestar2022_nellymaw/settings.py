932130e8c152a6ed9c747e12de45a3932414bc8d
Rodolfo Frainer <91473305+nellymaw@users.noreply.github.com>
2022-04-26 21:16:12 +0100

    mid transition to gitpod

--- codestar2022_nellymaw/settings.py
+++ codestar2022_nellymaw/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['codestar2022-nellymaw.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['codestar2022-nellymaw.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -40,11 +40,21 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'blog',
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
